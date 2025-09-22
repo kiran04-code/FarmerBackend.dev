32f5b343de616d961cb17fa4dc8078717151362e
kiran04-code <kr551344@gmail.com>
2025-09-22 14:54:52 +0530

    start work on bakend23

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import express from "express"
 import  {config} from "dotenv"
+import cors  from "cors"
 config();
 const app = express()
 const PORT = process.env.LOCAL_PORT || 8081;
+app.use(cors({
+    origin:"http://localhost:8081",
+    credentials:true
+}));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 app.get("/",(req,res)=>{
     res.send("server is Runing!")
+})
+
+app.get("/testApi",(req,res)=>{
+    console.log("done")
+    res.json({
+        name:"Kiran",
+        sirname:"rathod",
+    })
 })
 app.listen(PORT,(req,res)=>{
     console.log(`Local Server is Runing on Port:http://localhost:${PORT}`)
